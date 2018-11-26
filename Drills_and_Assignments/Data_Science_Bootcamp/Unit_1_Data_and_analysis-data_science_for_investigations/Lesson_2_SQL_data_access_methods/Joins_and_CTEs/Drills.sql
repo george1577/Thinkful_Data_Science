749f5c53d629011433d08a735aec10b7b2b08be5
George Chen <george1577@gmail.com>
2018-11-25 20:28:21 -0800

    Complete assignment Nov.25

--- Drills_and_Assignments/Data_Science_Bootcamp/Unit_1_Data_and_analysis-data_science_for_investigations/Lesson_2_SQL_data_access_methods/Joins_and_CTEs/Drills.sql
+++ Drills_and_Assignments/Data_Science_Bootcamp/Unit_1_Data_and_analysis-data_science_for_investigations/Lesson_2_SQL_data_access_methods/Joins_and_CTEs/Drills.sql
@@ -1,25 +1,23 @@
 -- What are the three longest trips on rainy days?
-WITH trip_plus
+WITH rain
 AS (
-SELECT (CASE WHEN start_date IS NOT NULL THEN SUBSTRING(start_date,1,10) END) AS date,
-        *
-FROM trips
+SELECT DISTINCT DATE(date) AS date
+FROM weather
+WHERE events = 'Rain'
 )
 SELECT t.*
-FROM trip_plus AS t
-LEFT JOIN weather AS w
-ON t.date = w.date
-WHERE w.events = 'Rain'
+FROM trips AS t
+JOIN rain AS r
+ON DATE(t.start_date) = r.date
 ORDER BY t.duration DESC
 LIMIT 3
 
-
-
-
-
-
-
-
+-- Which station is full most often?
+SELECT station_id,
+       SUM((CASE WHEN docks_available = 0 THEN 1 END)) AS empty_dock_freq
+FROM status
+GROUP BY station_id
+ORDER BY empty_dock_freq DESC
 
 -- Return a list of stations with a count of number of trips starting at that station but ordered by dock count.
 WITH station_trip
@@ -38,17 +36,16 @@ ON s.name = st.name
 ORDER BY st.dockcount
 
 -- (Challenge) What's the length of the longest trip for each day it rains anywhere?
-WITH trip_plus
+WITH rain
 AS (
-SELECT (CASE WHEN start_date IS NOT NULL THEN SUBSTRING(start_date,1,10)
-       END) AS date,
-        *
-FROM trips
+SELECT DISTINCT DATE(date) AS date
+FROM weather
+WHERE events = 'Rain'
 )
-SELECT t.date,
+SELECT DATE(t.start_date),
        MAX(duration) AS longest_trip_length
-FROM trip_plus AS t
-LEFT JOIN weather AS w
-ON t.date = w.date
-WHERE w.events LIKE 'Rain'
-GROUP BY t.date
+FROM trips AS t
+JOIN rain AS r
+ON DATE(t.start_date) = r.date
+GROUP BY DATE(t.start_date)
+ORDER BY longest_trip_length DESC
